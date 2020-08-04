6f7cddc6326d08318ed885e229d23da12a13c87d
Ilan Olkies <ilanolkies@outlook.com>
2020-08-04 19:02:06 -0300

    Add ipfs react native test

---
+++ ipfs/IPFSApp/App.js
@@ -0,0 +1,102 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TextInput,
+  Button
+} from 'react-native';
+
+import axios from 'axios'
+
+import {
+  Header,
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  const [hash, setHash] = useState('')
+  const [content, setContent] = useState('')
+
+  const get = () => axios.get('https://ipfs.io/ipfs/' + hash).then(res => res.status === 200 && res.data).then(setContent) //axios.get('https://ipfs.io/ipfs/' + hash).then(console.log).catch(console.error)
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Fetch file from IPFS</Text>
+              <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1 }} onChangeText={setHash} value={hash} />
+              <Button onPress={get} title="Get" />
+              <Text style={styles.sectionDescription}>
+                {content}
+              </Text>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
