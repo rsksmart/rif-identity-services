8b617c5cc358507d8a2c40b3faa5de4a4e156bf0
Ilan Olkies <ilanolkies@outlook.com>
2020-08-09 18:23:16 -0300

    Add database to data vault

--- data-vault/src/index.ts
+++ data-vault/src/index.ts
@@ -7,6 +7,9 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 
+/* data base */
+import { createConnection } from 'typeorm'
+
 /* token gen */
 import { randomBytes } from 'crypto'
 
@@ -18,7 +21,7 @@ import { createVerifiableCredentialJwt } from 'did-jwt-vc'
 import { verifyJWT } from 'did-jwt'
 
 /* Data vault */
-import { DataVaultProviderIPFS } from './lib/DataVaultProviderIPFS'
+import { DataVaultProviderIPFS, Entities } from './lib/DataVaultProviderIPFS'
 
 /* env */
 dotenv.config()
@@ -50,84 +53,91 @@ const identity = new EthrDID({
 /* setup auth */
 const authDictionary: any = {} // stores tokens and expiration time for given did
 
-/* setup data vault */
-const dataVaultProvider = new DataVaultProviderIPFS(
-  { host: 'localhost', port: env.ipfsPort, protocol: 'http' },
-  {}
-)
-
 debug(`Identity: ${identity.did}`)
-dataVaultProvider.get('did:ethr:0x4a795ab98dc3732d1123c6133d3efdc76d4c91f8')
+
+createConnection({
+  type: 'sqlite',
+  database: 'data-vault-mapper.sqlite',
+  entities: Entities,
+  synchronize: true,
+  logging: false
+}).then(connection => {
+  /* setup data vault */
+  const dataVaultProvider = new DataVaultProviderIPFS(
+    connection,
+    { host: 'localhost', port: env.ipfsPort, protocol: 'http' }
+  )
+  // here you can start to work with your entities
+
+  app.get('/identity', function(req, res) {
+    debug(`Requested identity`)
+    res.status(200).send(identity.did)
+  })
 
 
-app.get('/identity', function(req, res) {
-  debug(`Requested identity`)
-  res.status(200).send(identity.did)
-})
+  const authenticate = (jwt: string) => verifyJWT(jwt, {
+    resolver: didResolver
+  }).then(({ payload, issuer }) => {
+    if (authDictionary[issuer].token !== payload.claims.find((claim: any) => claim.claimType === 'token').claimValue) throw new Error('Invalid token')
+    if (authDictionary[issuer].exp < Math.floor(+new Date() / 1000)) throw new Error('Token expired')
+    return { payload, issuer }
+  })
 
+  app.post('/auth', bodyParser.json(), function(req, res) {
+    const { did } = req.body
+    const token = randomBytes(64).toString('hex')
+    debug(`${did} requested auth - token ${token}`)
 
-const authenticate = (jwt: string) => verifyJWT(jwt, {
-  resolver: didResolver
-}).then(({ payload, issuer }) => {
-  if (authDictionary[issuer].token !== payload.claims.find((claim: any) => claim.claimType === 'token').claimValue) throw new Error('Invalid token')
-  if (authDictionary[issuer].exp < Math.floor(+new Date() / 1000)) throw new Error('Token expired')
-  return { payload, issuer }
-})
+    const sub = did
+    const iss = identity.did
+    const nbf = Math.round((+Date.now()) / 1000)
+    const exp = Math.round((+Date.now() + parseInt(env.authExpirationTime)) / 1000)
+    const credentialSubject = { token }
 
-app.post('/auth', bodyParser.json(), function(req, res) {
-  const { did } = req.body
-  const token = randomBytes(64).toString('hex')
-  debug(`${did} requested auth - token ${token}`)
+    authDictionary[did] = { token, exp }
 
-  const sub = did
-  const iss = identity.did
-  const nbf = Math.round((+Date.now()) / 1000)
-  const exp = Math.round((+Date.now() + parseInt(env.authExpirationTime)) / 1000)
-  const credentialSubject = { token }
+    createVerifiableCredentialJwt({
+      sub,
+      iss,
+      nbf,
+      exp,
+      vc: {
+        '@context': ['https://www.w3.org/2018/credentials/v1'],
+        type: ['VerifiableCredential'],
+        credentialSubject
+      }
+    }, identity).then(jwt => res.status(200).send(jwt))
+  })
 
-  authDictionary[did] = { token, exp }
+  app.post('/testAuth', bodyParser.json(), function(req, res) {
+    const { jwt } = req.body
+    debug(`Testing auth`)
 
-  createVerifiableCredentialJwt({
-    sub,
-    iss,
-    nbf,
-    exp,
-    vc: {
-      '@context': ['https://www.w3.org/2018/credentials/v1'],
-      type: ['VerifiableCredential'],
-      credentialSubject
-    }
-  }, identity).then(jwt => res.status(200).send(jwt))
-})
+    authenticate(jwt)
+      .then(() => res.status(200).send('Authenticated'))
+      .catch(() => res.status(200).send('Not authenticated'))
+  })
 
-app.post('/testAuth', bodyParser.json(), function(req, res) {
-  const { jwt } = req.body
-  debug(`Testing auth`)
+  const authenticateAndFindClaim = (jwt: string) => (claimType: any) => authenticate(jwt)
+    .then(({ issuer, payload }) => ({ issuer, content: payload.claims.find((claim: any) => claim.claimType === claimType).claimValue }))
 
-  authenticate(jwt)
-    .then(() => res.status(200).send('Authenticated'))
-    .catch(() => res.status(200).send('Not authenticated'))
-})
+  app.post('/put', bodyParser.json(), function (req, res) {
+    debug(`Put`)
+    const { jwt } = req.body
 
-const authenticateAndFindClaim = (jwt: string) => (claimType: any) => authenticate(jwt)
-  .then(({ issuer, payload }) => ({ issuer, content: payload.claims.find((claim: any) => claim.claimType === claimType).claimValue }))
+    authenticateAndFindClaim(jwt)('content')
+      .then(({ issuer, content }) => dataVaultProvider.put(issuer, Buffer.from(content)))
+      .then(cid => res.status(200).send(cid))
+  })
 
-app.post('/put', bodyParser.json(), function (req, res) {
-  debug(`Put`)
-  const { jwt } = req.body
+  app.post('/get', bodyParser.json(), function (req, res) {
+    debug(`Get`)
+    const { jwt } = req.body
 
-  authenticateAndFindClaim(jwt)('content')
-    .then(({ issuer, content }) => dataVaultProvider.put(issuer, Buffer.from(content)))
-    .then(cid => res.status(200).send(cid))
-})
+    authenticate(jwt)
+      .then(({ issuer }) => dataVaultProvider.get(issuer))
+      .then(cids => res.status(200).send(JSON.stringify(cids)))
+  })
 
-app.post('/get', bodyParser.json(), function (req, res) {
-  debug(`Get`)
-  const { jwt } = req.body
-
-  authenticate(jwt)
-    .then(({ issuer }) => dataVaultProvider.get(issuer))
-    .then(cids => res.status(200).send(JSON.stringify(cids)))
-})
-
-app.listen(env.port, () => debug(`Data vault started on port ${env.port}`))
+  app.listen(env.port, () => debug(`Data vault started on port ${env.port}`))
+}).catch(error => console.log(error));
--- data-vault/src/lib/DataVaultProviderIPFS.ts
+++ data-vault/src/lib/DataVaultProviderIPFS.ts
@@ -1,5 +1,6 @@
 import RifStorage, { Provider } from '@rsksmart/rif-storage'
 import Debug from 'debug'
+import { Entity, PrimaryGeneratedColumn, Column, Connection } from 'typeorm'
 
 const debug = Debug('rif-id:data-vault:ipfs-provider')
 
@@ -8,19 +9,34 @@ interface IDataVaultProviderIPFS {
   get: (did: string) => Promise<string[]>
 }
 
+@Entity()
+class DataVaultEntry {
+  constructor(did: string, cid: string) {
+    this.did = did
+    this.cid = cid
+  }
+
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column('text')
+  did!: string;
+
+  @Column('text')
+  cid!: string;
+}
+
 const DataVaultProviderIPFS = (function (
   this: IDataVaultProviderIPFS,
+  dbConnection: Connection,
   ipfsOptions = { host: 'localhost', port: '8080', protocol: 'http' },
-  dbOptions = {}
 ) {
   const storage = RifStorage(Provider.IPFS, ipfsOptions)
 
-  /** TODO: use DB */
-  const hashDictionary: any = {}
-
-  function addToDictionary(did: string, cid: string) {
-    if (!hashDictionary[did]) hashDictionary[did] = []
-    hashDictionary[did].push(cid)
+  async function addToDictionary(did: string, cid: string) {
+    const entry = new DataVaultEntry(did, cid)
+    await dbConnection.manager.save(entry)
+    debug(`pair ${did} - ${cid} stored`)
   }
 
   /**
@@ -33,20 +49,25 @@ const DataVaultProviderIPFS = (function (
     const fileHash = await storage.put(content)
     debug('Stored hash: ' + fileHash)
 
-    await storage.ipfs.pin.add(fileHash)
+    await (storage as any).ipfs.pin.add(fileHash)
     debug('Pinned hash: ' + fileHash)
 
-    addToDictionary(did, fileHash)
+    await addToDictionary(did, fileHash)
 
     return fileHash
   }
 
-  this.get = async function(did: string) {
-    return hashDictionary[did] || []
+  this.get = function(did: string): Promise<string[]> {
+    return dbConnection.getRepository(DataVaultEntry).find({
+      where: { did },
+      select: ['cid']
+    }).then(entries => entries.map(entry => entry.cid))
   }
 } as any as { new (
-  ipfsOptions: { host: string, port: string, protocol: string },
-  dbOptions: any
+  dbOptions: Connection,
+  ipfsOptions: { host: string, port: string, protocol: string }
 ): IDataVaultProviderIPFS })
 
-export { DataVaultProviderIPFS }
+const Entities = [DataVaultEntry]
+
+export { DataVaultProviderIPFS, Entities }
