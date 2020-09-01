b373b0ea579942141dd79e457a494f5f72f0b0f1
Ilan <36084092+ilanolkies@users.noreply.github.com>
2020-08-31 22:04:17 -0300

    Change data vaut payloads (#25)
    * Put Data Vault service payloads in JWTs
    
    * Lint, fix tests and add circle

--- data-vault/src/lib/DataVaultProviderIPFS.ts
+++ data-vault/src/lib/DataVaultProviderIPFS.ts
@@ -1,16 +1,10 @@
-import RifStorage, { Provider } from '@rsksmart/rif-storage'
+import RIFStorage, { Provider } from '@rsksmart/rif-storage'
 import { Entity, PrimaryGeneratedColumn, Column, Connection } from 'typeorm'
 import logger from './logger'
 
-interface IDataVaultProviderIPFS {
-  put: (did: string, key: string, content: Buffer) => Promise<string>
-  get: (did: string, key: string) => Promise<string[]>
-  delete: (did: string, key: string, cid: string) => Promise<boolean>
-}
-
 @Entity()
 class DataVaultEntry {
-  constructor(did: string, key: string, cid: string) {
+  constructor (did: string, key: string, cid: string) {
     this.did = did
     this.key = key
     this.cid = cid
@@ -29,18 +23,27 @@ class DataVaultEntry {
   cid!: string;
 }
 
-const DataVaultProviderIPFS = (function (
-  this: IDataVaultProviderIPFS,
-  dbConnection: Connection,
-  ipfsOptions: IPFSOptions = { host: 'localhost', port: '8080', protocol: 'http' },
-) {
-  const storage = RifStorage(Provider.IPFS, ipfsOptions)
+type IPFSPinner = { ipfs: { pin: {
+  add: (cid: string) => Promise<void>
+  rm: (cid: string) => Promise<void>
+} } }
 
-  async function addToDictionary(did: string, key: string, value: string) {
-    const entry = new DataVaultEntry(did, key, value)
-    await dbConnection.manager.save(entry)
-    logger.info(`pair ${key} - ${value} stored for DID: ${did}`)
-  }
+type CentralizedIPFSPinnerProviderConfig = {
+  dbConnection: Connection,
+  ipfsOptions?: { host: string, port: string, protocol: string },
+}
+
+export interface ICentralizedIPFSPinnerProvider {
+  put: (did: string, key: string, content: Buffer) => Promise<string>
+  get: (did: string, key: string) => Promise<string[]>
+  delete: (did: string, key: string, cid: string) => Promise<boolean>
+}
+
+export const CentralizedIPFSPinnerProvider = (function (this: ICentralizedIPFSPinnerProvider, {
+  dbConnection,
+  ipfsOptions
+}: CentralizedIPFSPinnerProviderConfig) {
+  const storage = RIFStorage(Provider.IPFS, ipfsOptions || { host: 'localhost', port: '8080', protocol: 'http' })
 
   /**
    * Put a file in IPFS and associate it with a DID
@@ -48,35 +51,37 @@ const DataVaultProviderIPFS = (function (
    * @param {Buffer} content
    * @returns string
    */
-  this.put = async function(did: string, key: string, value: Buffer) {
-    const fileHash = await storage.put(value)
+  this.put = async function (did: string, key: string, content: Buffer) {
+    const fileHash = await storage.put(content)
     logger.info('Stored hash: ' + fileHash)
 
-    await (storage as any).ipfs.pin.add(fileHash)
+    await (storage as IPFSPinner).ipfs.pin.add(fileHash)
     logger.info('Pinned hash: ' + fileHash)
 
-    await addToDictionary(did, key, fileHash)
+    const entry = new DataVaultEntry(did, key, fileHash)
+    await dbConnection.manager.save(entry)
+    logger.info(`pair ${key} - ${content} stored for DID: ${did}`)
 
     return fileHash
   }
 
-  this.get = function(did: string, key: string): Promise<string[]> {
+  this.get = function (did: string, key: string): Promise<string[]> {
     return dbConnection.getRepository(DataVaultEntry).find({
       where: { did, key },
       select: ['cid']
-    }).then(entries => entries.map(entry => entry.cid))
+    }).then((entries: { cid: string }[]) => entries.map(entry => entry.cid))
   }
 
-  this.delete = async function(did: string, key: string, cid: string): Promise<boolean> {
+  this.delete = async function (did: string, key: string, cid: string): Promise<boolean> {
     const repository = dbConnection.getRepository(DataVaultEntry)
-    const file = await repository.findOne({ where: { did, key, cid } });
+    const file = await repository.findOne({ where: { did, key, cid } })
 
     if (file) {
-      const deleteResult =  await repository.remove(file)
+      const deleteResult = await repository.remove(file)
       logger.info(`Deleted hash: ${cid}`)
 
       if (!deleteResult.id) {
-        await (storage as any).ipfs.pin.rm(cid)
+        await (storage as IPFSPinner).ipfs.pin.rm(cid)
         logger.info(`Unpinned hash: ${cid}`)
 
         return true
@@ -85,17 +90,6 @@ const DataVaultProviderIPFS = (function (
 
     return false
   }
-} as any as { new (
-  dbOptions: Connection,
-  ipfsOptions: { host: string, port: string, protocol: string }
-): IDataVaultProviderIPFS })
+} as any as { new(config: CentralizedIPFSPinnerProviderConfig): ICentralizedIPFSPinnerProvider })
 
-const Entities = [DataVaultEntry]
-
-interface IPFSOptions {
-  host: string;
-  port: string;
-  protocol: string;
-}
-
-export { DataVaultProviderIPFS, Entities, IPFSOptions }
+export const Entities = [DataVaultEntry]
--- data-vault/src/services/centralizedIPFSPinner.ts
+++ data-vault/src/services/centralizedIPFSPinner.ts
@@ -16,7 +16,7 @@ import { createVerifiableCredentialJwt } from 'did-jwt-vc'
 import { verifyJWT } from 'did-jwt'
 
 /* Data vault */
-import { DataVaultProviderIPFS, Entities } from '../lib/DataVaultProviderIPFS'
+import { CentralizedIPFSPinnerProvider, Entities } from '../lib/DataVaultProviderIPFS'
 import logger from '../lib/logger'
 
 interface CentralizedIPFSPinnerEnv {
@@ -29,9 +29,23 @@ interface CentralizedIPFSPinnerEnv {
   dbFile: string;
 }
 
-export function setupCentralizedIPFSPinner(app: Express, env: CentralizedIPFSPinnerEnv, prefix = '') {
+export type DAFClaim = { claimType: string, claimValue: string }
+
+function findClaims (claims: DAFClaim[], claimTypes: string[]) {
+  const found: { [key: string]: string } = {}
+
+  for (const claim of claims) {
+    for (const claimType of claimTypes) {
+      if (claim.claimType === claimType) { found[claimType] = claim.claimValue }
+    }
+  }
+
+  return found
+}
+
+export function setupCentralizedIPFSPinner (app: Express, env: CentralizedIPFSPinnerEnv, prefix = ''): Promise<void> {
   /* setup did resolver */
-  const providerConfig = { networks: [{ name: "rsk:testnet", registry: "0xdca7ef03e98e0dc2b855be647c39abe984fcf21b", rpcUrl: env.rpcUrl }] }
+  const providerConfig = { networks: [{ name: 'rsk:testnet', registry: '0xdca7ef03e98e0dc2b855be647c39abe984fcf21b', rpcUrl: env.rpcUrl }] }
   const ethrDidResolver = getResolver(providerConfig)
   const didResolver = new Resolver(ethrDidResolver)
 
@@ -41,7 +55,8 @@ export function setupCentralizedIPFSPinner(app: Express, env: CentralizedIPFSPin
   })
 
   /* setup auth */
-  const authDictionary: any = {} // stores tokens and expiration time for given did
+  // TODO: store in DB
+  const authDictionary: { [issuer: string]: { token: string, exp: number } } = {} // stores tokens and expiration time for given did
 
   logger.info(`Identity: ${identity.did}`)
 
@@ -51,33 +66,33 @@ export function setupCentralizedIPFSPinner(app: Express, env: CentralizedIPFSPin
     entities: Entities,
     synchronize: true,
     logging: false
-  }).then(connection => {
+  }).then(dbConnection => {
     /* setup data vault */
-    const dataVaultProvider = new DataVaultProviderIPFS(
-      connection,
-      { host: env.ipfsHost, port: env.ipfsPort, protocol: 'http' }
-    )
+    const dataVaultProvider = new CentralizedIPFSPinnerProvider({
+      dbConnection,
+      ipfsOptions: { host: env.ipfsHost, port: env.ipfsPort, protocol: 'http' }
+    })
 
     /* authentication */
     const authenticate = (jwt: string) => verifyJWT(jwt, {
       resolver: didResolver
     }).then(({ payload, issuer }) => {
-      if (authDictionary[issuer].token !== payload.claims.find((claim: any) => claim.claimType === 'token').claimValue) throw new Error('Invalid token')
+      if (authDictionary[issuer].token !== payload.claims.find((claim: DAFClaim) => claim.claimType === 'token').claimValue) throw new Error('Invalid token')
       if (authDictionary[issuer].exp < Math.floor(+new Date() / 1000)) throw new Error('Token expired')
       return { payload, issuer }
     })
 
-    const authenticateAndFindClaim = (jwt: string) => (claimType: any) => authenticate(jwt)
-      .then(({ issuer, payload }) => ({ issuer, content: payload.claims.find((claim: any) => claim.claimType === claimType).claimValue }))
+    const authenticateAndFindClaims = (jwt: string) => (claimTypes: string[]) => authenticate(jwt)
+      .then(({ issuer, payload }) => ({ issuer, claims: findClaims(payload.claims, claimTypes) }))
 
-    app.get(prefix + '/identity', function(req, res) {
-      logger.info(`Requested identity`)
+    app.get(prefix + '/identity', function (req, res) {
+      logger.info('Requested identity')
       res.status(200).send(identity.did)
     })
 
-    app.use(bodyParser.json({ limit: '50kb'} ))
+    app.use(bodyParser.json({ limit: '50kb' }))
 
-    app.post(prefix + '/auth', function(req, res) {
+    app.post(prefix + '/auth', function (req, res) {
       const { did } = req.body
       const token = randomBytes(64).toString('hex')
       logger.info(`${did} requested auth - token ${token}`)
@@ -103,9 +118,9 @@ export function setupCentralizedIPFSPinner(app: Express, env: CentralizedIPFSPin
       }, identity).then(jwt => res.status(200).send(jwt))
     })
 
-    app.post(prefix + '/testAuth', function(req, res) {
+    app.post(prefix + '/testAuth', function (req, res) {
       const { jwt } = req.body
-      logger.info(`Testing auth`)
+      logger.info('Testing auth')
 
       authenticate(jwt)
         .then(() => res.status(200).send('Authenticated'))
@@ -114,29 +129,29 @@ export function setupCentralizedIPFSPinner(app: Express, env: CentralizedIPFSPin
 
     /* operations */
     app.post(prefix + '/put', function (req, res) {
-      logger.info(`Put`)
-      const { jwt, key } = req.body
+      logger.info('Put')
+      const { jwt } = req.body
 
-      authenticateAndFindClaim(jwt)('content')
-        .then(({ issuer, content }) => dataVaultProvider.put(issuer, key, Buffer.from(content)))
+      authenticateAndFindClaims(jwt)(['key', 'content'])
+        .then(({ issuer, claims }) => dataVaultProvider.put(issuer, claims.key, Buffer.from(claims.content)))
         .then(cid => res.status(200).send(cid))
     })
 
     app.post(prefix + '/get', function (req, res) {
-      logger.info(`Get`)
-      const { jwt, key } = req.body
+      logger.info('Get')
+      const { jwt } = req.body
 
-      authenticate(jwt)
-        .then(({ issuer }) => dataVaultProvider.get(issuer, key))
+      authenticateAndFindClaims(jwt)(['key'])
+        .then(({ issuer, claims }) => dataVaultProvider.get(issuer, claims.key))
         .then(cids => res.status(200).send(JSON.stringify(cids)))
     })
 
     app.post(prefix + '/delete', function (req, res) {
-      logger.info(`Delete`)
-      const { jwt, key, cid } = req.body
+      logger.info('Delete')
+      const { jwt } = req.body
 
-      authenticate(jwt)
-        .then(({ issuer }) => dataVaultProvider.delete(issuer, key, cid))
+      authenticateAndFindClaims(jwt)(['key', 'cid'])
+        .then(({ issuer, claims }) => dataVaultProvider.delete(issuer, claims.key, claims.cid))
         .then(() => res.status(204).send())
     })
 
